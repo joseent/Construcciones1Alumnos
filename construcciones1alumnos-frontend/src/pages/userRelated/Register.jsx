31c87028540c334d70f73ca4c17dd91eeeffe2fe
jose E Nieva Toppa <jose.ent17@gmail.com>
2020-05-28 17:16:59 -0300

    RegisterPageCSS

--- construcciones1alumnos-frontend/src/pages/userRelated/Register.jsx
+++ construcciones1alumnos-frontend/src/pages/userRelated/Register.jsx
@@ -75,7 +75,7 @@ export default function Register() {
     <div className="w-full flex justify-center items-center">
       <form
         onSubmit={handleSubmit}
-        className="mediaform w-1/2 bglayer1 rounded-md p-6 my-1"
+        className="mediafullwidth w-1/2 bglayer1 rounded-md p-6 my-1"
       >
         <div className="flex flex-wrap -mx-3 mb-6">
           <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
